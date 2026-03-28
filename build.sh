82e569441199f1379e5c795c643849b71be6cee8
SUDIPTO-2005 <sudiptobhadra9c.jssp@gmail.com>
2026-03-28 17:06:28 +0530

    Load production data

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py migrate
-python manage.py loaddata data.json
-
+python manage.py loaddata data.json || true
 python manage.py collectstatic --noinput
