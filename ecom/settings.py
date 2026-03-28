c289140cea259bcc4aa66d2c65140195ebe83f44
SUDIPTO-2005 <sudiptobhadra9c.jssp@gmail.com>
2026-03-28 16:55:32 +0530

    Enable debug for fixing error

--- ecom/settings.py
+++ ecom/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-fallback-secret-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
