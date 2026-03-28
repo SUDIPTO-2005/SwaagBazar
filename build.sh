87cc6562cae0519eb9c863bfe4a01a063b9f9065
SUDIPTO-2005 <sudiptobhadra9c.jssp@gmail.com>
2026-03-28 16:19:40 +0530

    Load data on deploy

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+python manage.py loaddata data.json
+
+python manage.py collectstatic --noinput
